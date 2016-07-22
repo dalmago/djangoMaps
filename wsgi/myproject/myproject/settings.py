65d4b1a88b4de23e9cf8bb6fc55b4e24d5278bfb
Matheus Dal Mago <matheusdalmago10@hotmail.com>
2016-07-22 12:30:01 -0300

    fix static path

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
+        os.path.join(BASE_DIR, "react/static"),
 ]
 
 LEAFLET_CONFIG = {
